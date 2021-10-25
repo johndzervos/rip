2942763d32dea8da62ddba862651f62a23c55d8a
johndzervos <ioannis@crunchrapps.com>
2021-10-25 23:44:52 +0200

    First commit

---
+++ markers/admin.py
@@ -0,0 +1,9 @@
+"""Markers admin."""
+from django.contrib.gis import admin
+from .models import Marker
+
+
+@admin.register(Marker)
+class MarkerAdmin(admin.OSMGeoAdmin):
+    """Marker admin."""
+    list_display = ("name", "location")
---
+++ markers/models.py
@@ -0,0 +1,11 @@
+"""Markers models."""
+
+from django.contrib.gis.db.models import PointField
+from django.db import models
+
+class Marker(models.Model):
+  """
+  A marker with name and location.
+  """
+  name = models.CharField(max_length=255)
+  location = PointField()
