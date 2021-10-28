465da1e8fed1481cb2155dc8c6aadf0fef214004
johndzervos <ioannis@crunchrapps.com>
2021-10-28 23:14:28 +0200

    Load markers in the screen. https://www.paulox.net/2021/07/19/maps-with-django-part-2-geodjango-postgis-and-leaflet/

---
+++ markers/api_urls.py
@@ -0,0 +1,7 @@
+"""Markers API URL Configuration."""
+from rest_framework import routers
+from markers.api_views import MarkerViewSet
+
+router = routers.DefaultRouter()
+router.register(r"markers", MarkerViewSet)
+urlpatterns = router.urls
--- markers/static/map.js
+++ markers/static/map.js
@@ -1,6 +1,19 @@
-const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-const map = L.map('map')
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: attribution }).addTo(map);
-const markers = JSON.parse(document.getElementById('markers-data').textContent);
-let feature = L.geoJSON(markers).bindPopup(function (layer) { return layer.feature.properties.name; }).addTo(map);
-map.fitBounds(feature.getBounds(), { padding: [100, 100] });
+const copy = '© <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
+const url = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+const osm = L.tileLayer(url, { attribution: copy })
+const map = L.map('map', { layers: [osm], minZoom: 5 })
+map.locate()
+  .on('locationfound', e => map.setView(e.latlng, 8))
+  .on('locationerror', () => map.setView([0, 0], 5))
+
+async function load_markers() {
+  const markers_url = `/api/markers/?in_bbox=${map.getBounds().toBBoxString()}`
+  const response = await fetch(markers_url)
+  const geojson = await response.json()
+  return geojson
+}
+async function render_markers() {
+  const markers = await load_markers()
+  L.geoJSON(markers).bindPopup(layer => layer.feature.properties.name).addTo(map)
+}
+map.on('moveend', render_markers)
